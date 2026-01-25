3ee62a041820e7b80395645e45c1cd3d49839426
Zxzidan <dandiazaidane05@gmail.com>
2026-01-25 11:16:11 +0700

    Operator perbandingan

---
+++ dart_operator_perbandingan.dart
@@ -0,0 +1,19 @@
+/// Operator Perbandingan
+/// == (sama dengan)
+/// != (Tidak sama dengan)
+/// > (Lebih dari)
+/// < (Kurang dari)
+/// >= (Lebih dari atau sama dengan)
+/// <= (Kurang dari atau sama dengan)
+
+void main() {
+  int data1 = 250;
+  double data2 = 270.19;
+
+  print(data1 == data2);
+  print(data1 != data2);
+  print(data1 > data2);
+  print(data1 < data2);
+  print(data1 >= data2);
+  print(data1 <= data2);
+}

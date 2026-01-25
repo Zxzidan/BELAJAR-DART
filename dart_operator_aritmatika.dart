e61159d0efa94223097b6dfe46aa1373fd927e43
Zxzidan <dandiazaidane05@gmail.com>
2026-01-25 11:07:59 +0700

    operator aritmatika

---
+++ dart_operator_aritmatika.dart
@@ -0,0 +1,19 @@
+/// Operator aritmatika
+/// + = Pertambahan
+/// - , -expression = Pengurangan atau negatif
+/// * = perkalian
+/// / = Pembagian, Hasil double
+/// ~/ = Pembagian, hasil int
+/// % = Sisa bagi
+
+void main() {
+  int data1 = 200;
+  double data2 = 157.5;
+
+  print(data1 + data2);
+  print(data1 - data2);
+  print(data1 * data2);
+  print(data1 / data2);
+  print(data1 ~/ data2);
+  print(data1 % data2);
+}

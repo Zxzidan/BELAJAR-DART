24f21028624b5d234b174bc0a1694e0c43556706
Zxzidan <dandiazaidane05@gmail.com>
2026-01-27 13:31:38 +0700

    baru

--- dart_Operator_penugasan.dart
+++ dart_Operator_penugasan.dart
@@ -1,6 +1,19 @@
 void main() {
+  // OPerator Penugasan
   num a = 200;
 
-  a += 267.98;
+  a += 267.98; // a = a + 267.98
   print(a);
+
+  var i = 5;
+  i++; // i = i+1
+  print('Untuk Menampilkan variabel i : $i');
+
+  var b = 0;
+  var j = b++; // j = b, b++
+  var k = ++b; // k = ++b
+
+  print('Ini untuk menampilkan variabel b : $b');
+  print('Ini untuk menmapilkan variabel j : $j');
+  print('Ini untuk menampilkan variabel k : $k');
 }

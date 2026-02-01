a0a11bc07bf2a99cdf5b900430d2d765eeac3eea
Zxzidan <dandiazaidane05@gmail.com>
2026-02-01 16:33:12 +0700

    ternary operator

---
+++ dart_ternary_operator.dart
@@ -0,0 +1,14 @@
+// Contoh penggunaan operator ternary di Dart. Pengganti dari if-else sederhana.
+
+void main() {
+  var nilaiMahasiswa = 94;
+  String hasilKelulusan = (nilaiMahasiswa >= 75) ? "Lulus" : "Tidak Lulus";
+
+  // if (nilaiMahasiswa >= 75) {
+  //   hasilKelulusan = "Lulus";
+  // } else {
+  //   hasilKelulusan = "Tidak Lulus";
+  // }
+
+  print(hasilKelulusan);
+}

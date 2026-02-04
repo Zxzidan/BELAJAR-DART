e7d3c3b29eeaa998f9036eac4ae22cc0865f985e
Zxzidan <dandiazaidane05@gmail.com>
2026-02-04 09:02:06 +0700

    for tanpa henti

---
+++ dart_for_tanpa_henti.dart
@@ -0,0 +1,25 @@
+/*
+
+Sintak Perulangan For
+
+for (init statement; condition; post statement) {
+  // kode yang akan diulang
+}
+
+1. Init statement akan dieksekusi sekali di awal perulangan.
+2. condition akan dilakukan pengecekan dalam setiap perulangan, jika
+true perulangan akan dilakukan, jika false perulangan akan dihentikan.
+3. Post statement akan dieksekusi setiap kali akhir perulangan.
+4. Init statement, condition, dan post statment tidak wajib diisi, jika kondisi
+tidak diisi, berarti kondisi selalu bernilai true.
+
+ */
+
+void main() {
+  
+  // Contoh perulangan tanpa henti
+  for (;;) {
+    print("Aku Dandi Azaidane");
+  
+  }
+}

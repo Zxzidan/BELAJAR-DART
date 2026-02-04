9eb6b27ef02e67c5cac0c7190a8376c65a766a39
Zxzidan <dandiazaidane05@gmail.com>
2026-02-04 13:24:56 +0700

    dart function

---
+++ dart_function.dart
@@ -0,0 +1,21 @@
+/*
+1. Function adalah blok kode program yang akan berjalan saat dipanggil.
+2. Sebelumnya kita sudah menggunakan method print() untuk menampilkan tulisan
+di console.
+3. Untuk membuat function, kita bisa menggunakan kata kunci void, lalu diikuti
+dengan nama function, kurung () dan diakhiri dengan block.
+4. Kita bisa memanggil function dengan menggunakan nama function lalu diikuti
+dengan kurung ().
+5. Di bahasa pemrograman lain, function juga disebut dengan method.
+
+ */
+
+void sayHello() {
+  print('Hello Function');
+}
+
+void main (){
+sayHello();
+sayHello();
+
+}

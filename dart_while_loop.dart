97973a7e52765921e99c629c49c2be06b9d1a42c
Zxzidan <dandiazaidane05@gmail.com>
2026-02-04 09:21:06 +0700

    while loop

---
+++ dart_while_loop.dart
@@ -0,0 +1,20 @@
+/*
+
+1. While loop adalag versi perulangan
+yang lebih sederhana dibandingkan for loop.
+
+2. Di while loop, hanya terdapat kondisi perulangan,
+tanpa ada init statement dan post statement.
+
+ */
+
+void main (){
+
+var counter = 1;
+
+  while (counter <= 10) {
+    print("Ini adalah perulangan ke-$counter");
+
+    counter++;
+  }
+}
